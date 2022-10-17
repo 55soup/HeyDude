f18a9a0905ee8a16aad7f166b648feb70352b340
Park Sunjoo <w2118@e-mirim.hs.kr>
2022-10-18 00:00:27 +0900

    recyclerview 연습

---
+++ app/src/main/java/com/example/hey_dude/FriendItem.java
@@ -0,0 +1,37 @@
+package com.example.hey_dude;
+
+public class FriendItem {
+    String name;
+    String today;
+    int resourceId;
+
+    public FriendItem(String name, String today, int resourceId) {
+        this.name = name;
+        this.today = today;
+        this.resourceId = resourceId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getToday() {
+        return today;
+    }
+
+    public int getResourceId() {
+        return resourceId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setToday(String today) {
+        this.today = today;
+    }
+
+    public void setResourceId(int resourceId) {
+        this.resourceId = resourceId;
+    }
+}
