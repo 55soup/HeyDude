a89de56653a2883ebbbeaf2a2cfb9de456b44557
Park Sunjoo <w2118@e-mirim.hs.kr>
2022-10-30 06:16:41 +0900

    navbar 조작가능(프래그먼트 추가), 디자인 수정

--- app/src/main/java/com/mirim/hey_dude/NavBar.java
+++ app/src/main/java/com/mirim/hey_dude/NavBar.java
@@ -1,5 +1,6 @@
 package com.mirim.hey_dude;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.ImageView;
@@ -11,7 +12,9 @@ import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.example.hey_dude.R;
+import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.bottomnavigation.LabelVisibilityMode;
 import com.google.android.material.navigation.NavigationBarView;
 
 public class NavBar extends AppCompatActivity {
@@ -19,6 +22,7 @@ public class NavBar extends AppCompatActivity {
     Fragment2 fragment2;
     Fragment3 fragment3;
     Fragment4 fragment4;
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +33,35 @@ public class NavBar extends AppCompatActivity {
         fragment2 = new Fragment2();
         fragment3 = new Fragment3();
         fragment4 = new Fragment4();
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
 
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.alarm:
+                        Toast.makeText(getApplicationContext(),"첫번째",Toast.LENGTH_SHORT).show();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragment1).commit();
+                        return true;
+
+                    case R.id.micro:
+                        Toast.makeText(getApplicationContext(),"두번째",Toast.LENGTH_SHORT).show();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,fragment2).commit();
+                        return true;
+
+                    case R.id.addDude:
+                        Toast.makeText(getApplicationContext(),"세번째",Toast.LENGTH_SHORT).show();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,fragment3).commit();
+                        return true;
+
+                    case R.id.profile:
+                        Toast.makeText(getApplicationContext(),"네번째",Toast.LENGTH_SHORT).show();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container,fragment4).commit();
+                        return true;
+                }
+                return false;
+            }
+        });
 
     }
 }
