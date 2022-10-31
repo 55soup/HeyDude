f39c09eb03c39f19a70dc2d5c829c08a63c6c02a
HA JIN <w2128@e-mirim.hs.kr>
2022-10-31 22:31:28 +0900

    SetAlarmActivity 화면 전환 애니메이션 추가

---
+++ app/src/main/java/com/mirim/hey_dude/SetAlarmActivity.kt
@@ -0,0 +1,28 @@
+package com.mirim.hey_dude
+
+import android.content.Context
+import android.content.Intent
+import android.icu.number.Scale.none
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.hey_dude.R
+
+class SetAlarmActivity : AppCompatActivity() {
+//    private lateinit var binding : ActivityDetailBinding
+    companion object {
+        fun newIntent(context: Context): Intent {
+            return Intent(context, SetAlarmActivity::class.java)
+        }
+    }
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_set_alarm)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        
+        overridePendingTransition(R.anim.slide_up_enter, R.anim.slide_down_exit)
+    }
+}
