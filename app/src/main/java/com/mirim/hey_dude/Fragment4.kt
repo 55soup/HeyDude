3ed8ddba08c630134dcfd8fdf982a755d7c3ab97
Park Sunjoo <w2118@e-mirim.hs.kr>
2022-11-03 13:24:51 +0900

    fragment4 코틀린 파일 생성

--- app/src/main/java/com/mirim/hey_dude/Fragment4.kt
+++ app/src/main/java/com/mirim/hey_dude/Fragment4.kt
@@ -1,10 +1,12 @@
 package com.mirim.hey_dude
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import com.example.hey_dude.R
 
 // TODO: Rename parameter arguments, choose names that match
@@ -21,12 +23,14 @@ class Fragment4 : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private var btnSet: ImageButton? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
+
         }
     }
 
@@ -34,8 +38,16 @@ class Fragment4 : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val view: View = inflater.inflate(R.layout.activity_fragment4, container, false)
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.activity_fragment4, container, false)
+
+        btnSet?.setOnClickListener {
+                view -> Log.d("mytag","btnSet 클릭")
+        }
+
+        return view
+
+
     }
 
     companion object {
@@ -57,4 +69,5 @@ class Fragment4 : Fragment() {
                 }
             }
     }
+
 }
