68b2429fa7061e5a46211584fa665584f3adfa50
Park Sunjoo <w2118@e-mirim.hs.kr>
2022-11-07 21:42:46 +0900

    update

--- app/src/main/java/com/mirim/hey_dude/MainActivity2.java
+++ app/src/main/java/com/mirim/hey_dude/MainActivity2.java
@@ -9,6 +9,7 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.core.view.ViewCompat;
 import androidx.core.widget.NestedScrollView;
 import androidx.fragment.app.FragmentManager;
@@ -21,10 +22,13 @@ import com.google.android.material.bottomnavigation.BottomNavigationMenuView;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.bottomnavigation.LabelVisibilityMode;
 import com.google.android.material.navigation.NavigationBarView;
+import com.mirim.hey_dude.friendRecyclerView.FriendItem;
+import com.mirim.hey_dude.friendRecyclerView.MyRecyclerAdapter;
 
 import java.util.ArrayList;
 
 public class MainActivity2 extends AppCompatActivity {
+    SearchView search_view;
     RecyclerView mRecyclerView;
     private ArrayList<FriendItem> mFriendList;
     private MyRecyclerAdapter myRecyclerAdapter;
@@ -33,26 +37,35 @@ public class MainActivity2 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
-        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-        mFriendList = new ArrayList<>();
+        mRecyclerView = findViewById(R.id.recyclerView);
+        search_view = findViewById(R.id.search_view);
+        search_view.clearFocus();
+        search_view.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
 
-        for (int i = 0; i <5; i++) {
-            addItem("iconName", "taek" + i, "taek2.tistory.com");
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mFriendList = new ArrayList<>();
+        
+        // item 추가
+        for (int i = 0; i < 20; i++) {
+            mFriendList.add(new FriendItem("우우우", "우우웅", "R.drawable.ic_baseline_add_24"));
         }
+        mFriendList.add(new FriendItem("에엥", "엉", "R.drawable.ic_baseline_add_24"));
 
         myRecyclerAdapter = new MyRecyclerAdapter(mFriendList);
         mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
         mRecyclerView.setAdapter(myRecyclerAdapter);
 
     }
-    public void addItem(String imgName, String name, String today){
-        FriendItem item = new FriendItem();
-
-        item.setName(name);
-        item.setResourceId(imgName);
-        item.setToday(today);
-    }
+   
 
 }
-
-
