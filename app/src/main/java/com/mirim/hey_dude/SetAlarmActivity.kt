5d1b5e48a33e6c82a0a4fe844aa322b1452cba80
HA JIN <w2128@e-mirim.hs.kr>
2022-11-08 23:47:34 +0900

    SwitchCompat 커스텀, 알람 추가 레이아웃 완료

--- app/src/main/java/com/mirim/hey_dude/SetAlarmActivity.kt
+++ app/src/main/java/com/mirim/hey_dude/SetAlarmActivity.kt
@@ -5,9 +5,23 @@ import android.content.Intent
 import android.icu.number.Scale.none
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Switch
+import android.widget.ToggleButton
+import androidx.appcompat.widget.SwitchCompat
 import com.example.hey_dude.R
 //알람 설정 페이지
 class SetAlarmActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_set_alarm)
+
+        //다시 알람 토글 버튼 이벤트트
+        val switch: SwitchCompat = findViewById(R.id.switchView)
+        switch.setOnCheckedChangeListener { compoundButton, ischecked ->  }
+
+    }
 
     companion object {
         fun newIntent(context: Context): Intent {
@@ -15,14 +29,12 @@ class SetAlarmActivity : AppCompatActivity() {
         }
 
     }
-    
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_set_alarm)
-    }
 
     override fun onBackPressed() {
         super.onBackPressed()
         overridePendingTransition(R.anim.slide_up_enter, R.anim.slide_down_exit)
     }
+
+
+
 }
