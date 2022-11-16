712527cde03be8a017a90a71fbcbc270ff24b316
HA JIN <w2128@e-mirim.hs.kr>
2022-11-16 21:34:57 +0900

    AlarmAdapter 삭제, build.gradle 의존성 추가, SetAlarm 레이아웃 요소 불러오기

--- app/src/main/java/com/mirim/hey_dude/Login/Config.kt
+++ app/src/main/java/com/mirim/hey_dude/Login/Config.kt
@@ -3,9 +3,9 @@ package com.mirim.hey_dude.Login
 class Config {
     companion object {
         // 데이터베이스 주소
-        val DATABASE_URL = "https://hey-dude-2b076-default-rtdb.firebaseio.com/"
+        val DATABASE_URL = "https://fir-study-fbe49-default-rtdb.asiasoutheast1.firebasedatabase.app/"
         // 구글 로그인을 위한 CLIENT ID와 RESULT 정수
-        val GOOGLE_SIGN_IN_WEB_CLIENT_ID = "789290372757-h00au8ef4chhg2lnkphvum9f2ivohe10.apps.googleusercontent.com"
+        val GOOGLE_SIGN_IN_WEB_CLIENT_ID = "222582836991-ptor9u83uogs4ts60n1inja6r7fm52ma.apps.googleusercontent.com"
         val RC_SIGN_IN = 12345
     }
 
