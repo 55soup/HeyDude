e35de10e95bf3f04d74b748a42026a25e3886435
HA JIN <w2128@e-mirim.hs.kr>
2022-11-17 16:13:49 +0900

    alarmBuilder, alarm

--- app/src/main/java/com/mirim/hey_dude/alarm/Alarm.java
+++ app/src/main/java/com/mirim/hey_dude/alarm/Alarm.java
@@ -1,4 +1,58 @@
 package com.mirim.hey_dude.alarm;
 
-public class Alarm {
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.io.Serializable;
+
+@Entity(tableName = "alarm_table")
+public class Alarm implements Serializable {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String label;
+    private int hour;
+    private int minute;
+    private boolean repeatFlag;
+
+    public Alarm() {}
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public String getTitle() {
+        return label;
+    }
+
+    public void setTitle(String title) {
+        this.label = title;
+    }
+
+    public boolean isRepeatFlag() {
+        return repeatFlag;
+    }
+
+    public void setRepeatFlag(boolean repeatFlag) {
+        this.repeatFlag = repeatFlag;
+    }
 }
--- app/src/main/java/com/mirim/hey_dude/alarm/AlarmBuilder.java
+++ app/src/main/java/com/mirim/hey_dude/alarm/AlarmBuilder.java
@@ -1,4 +1,35 @@
 package com.mirim.hey_dude.alarm;
 
 public class AlarmBuilder {
+    Alarm alarm;
+
+    public AlarmBuilder() {
+        alarm = new Alarm();
+    }
+    public AlarmBuilder setId(int id) {
+        alarm.setId(id);
+        return this;
+    }
+
+    public AlarmBuilder setHour(int hour) {
+        alarm.setHour(hour);
+        return this;
+    }
+
+    public AlarmBuilder setMinute(int minute) {
+        alarm.setMinute(minute);
+        return this;
+    }
+
+    public AlarmBuilder setLabel(String label) {
+        alarm.setTitle(label);
+        return this;
+    }
+
+    public AlarmBuilder setTotalFlag(boolean repeatFlag) {
+        alarm.setRepeatFlag(repeatFlag);
+        return this;
+    }
+
+    public Alarm build() { return this.alarm; }
 }
